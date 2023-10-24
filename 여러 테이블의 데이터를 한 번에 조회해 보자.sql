760b2549e06bdf1acc05995f43726b2b040763ce
xoyeon <syyang@sookmyung.ac.kr>
2023-10-24 12:57:53 +0900

    DAY9

--- 여러 테이블의 데이터를 한 번에 조회해 보자.sql
+++ 여러 테이블의 데이터를 한 번에 조회해 보자.sql
@@ -31,6 +31,34 @@ VALUES (26, 'raichu', 'electric', 80, 60),
 (470, 'leafeon', 'grass', 110, 130);
 
 # MISSION (1) 내 포켓몬과 친구의 포켓몬에 어떤 타입들이 있는지 중복 제외하고 같은 타입은 한 번 씩만 가져와 주세요.
+select type
+from mypokemon
+union
+select type
+from friendpokemon;
 
+select type
+from mypokemon
+union
+select distinct type
+from friendpokemon;
 
 # MISSION (2) 내 포켓몬과 친구의 포켓몬 중에 풀(grass) 타입 포켓몬들의 포켓몬 번호와 이름을 중복 포함하여 전부 다 가져와 주세요.
+select number, name, 'mine' as whose
+from mypokemon
+where type = 'grass'
+union all
+select number, name, "friend's" as whose
+from friendpokemon
+where type = 'grass';
+
+# MISSION (1) 나도 가지고 있고, 친구도 가지고 있는 포켓몬의 이름을 가져와 주세요. -- 교집합 개념 사용
+select mypokemon.name
+from mypokemon inner join friendpokemon
+on mypokemon.number = friendpokemon.number;
+
+# MISSION (2) 나만 가지고 있고, 친구는 안 가지고 있는 포켓몬의 이름을 가져와 주세요. -- 차집합 개념 사용(내 포켓몬 - 친구 포켓몬)
+select mypokemon.name
+from mypokemon left join friendpokemon
+on mypokemon.number = friendpokemon.number and mypokemon.number = friendpokemon.number
+where friendpokemon.number is null;
